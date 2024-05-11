deae563f631c351b1ac0070d4ecab324baeb5590
juhyun <wngus5861@naver.com>
2024-05-11 12:44:09 +0900

    컴포넌트 나열 및 경로 수정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Gallery from './components/Gallery/Gallery';
 import Blog from './components/Blog/Blog';
 function App() {
-  <div>
-    <Blog/>
-    <Gallery/>
-  </div>
+  return(
+    <div>
+      <Gallery/>
+      <Blog/>
+    </div>
+  );
 }
 export default App;
